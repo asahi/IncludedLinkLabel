cd069849873b59ee7bb891b0fe16fc059ea7f1cb
asahijung <asahijung@gmail.com>
2013-02-10 22:42:05 +0900

    Create project

---
+++ IncludedLinkLabel.h
@@ -0,0 +1,13 @@
+//
+//  IncludedLinkLabel.h
+//  IncludedLinkLabel
+//
+//  Created by Jung Giuk on 2013/02/10.
+//  Copyright (c) 2013年 Jung Giuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IncludedLinkLabel : NSObject
+
+@end
---
+++ IncludedLinkLabel.m
@@ -0,0 +1,13 @@
+//
+//  IncludedLinkLabel.m
+//  IncludedLinkLabel
+//
+//  Created by Jung Giuk on 2013/02/10.
+//  Copyright (c) 2013年 Jung Giuk. All rights reserved.
+//
+
+#import "IncludedLinkLabel.h"
+
+@implementation IncludedLinkLabel
+
+@end
