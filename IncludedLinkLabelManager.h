0456564a19b0d2cb514163e95bd24872ee2cd7d1
asahijung <asahijung@gmail.com>
2013-02-13 23:34:08 +0900

    Refactoring : Add Class for support IncludedLinkLabel
    - IncludedLinkLabelManager

---
+++ IncludedLinkLabelManager.h
@@ -0,0 +1,13 @@
+//
+//  IncludedLinkLabelManager.h
+//  IncludedLinkLabel
+//
+//  Created by Jung Giuk on 2013/02/13.
+//  Copyright (c) 2013年 Jung Giuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IncludedLinkLabelManager : NSObject
+
+@end
---
+++ IncludedLinkLabelManager.m
@@ -0,0 +1,13 @@
+//
+//  IncludedLinkLabelManager.m
+//  IncludedLinkLabel
+//
+//  Created by Jung Giuk on 2013/02/13.
+//  Copyright (c) 2013年 Jung Giuk. All rights reserved.
+//
+
+#import "IncludedLinkLabelManager.h"
+
+@implementation IncludedLinkLabelManager
+
+@end
