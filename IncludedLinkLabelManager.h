3f6e9b674416475006f907361a37102139bc2ddb
asahijung <asahijung@gmail.com>
2013-02-13 23:35:53 +0900

    Add new class for Method managerment

--- IncludedLinkLabelManager.h
+++ IncludedLinkLabelManager.h
@@ -8,6 +8,9 @@
 
 #import <Foundation/Foundation.h>
 
-@interface IncludedLinkLabelManager : NSObject
+@class IncludedLinkLabel;
 
+@interface IncludedLinkLabelManager : NSObject
++ (NSDictionary *)nsAttributedStringAttributesFromLabel:(IncludedLinkLabel *)label;
++ (NSAttributedString *)nsAttributedStringBySettingColorFromContext:(NSAttributedString *)attributedString color:(UIColor *)color;
 @end
